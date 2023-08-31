de3c266d9221a7453fe5e7132bca08b6856e1289
Melihcan Cilek <melihcanclk@hotmail.com>
2023-08-31 13:43:19 +0300

    feat(postgres): creating postgres image in compose and connecting to database

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/melihcanclk/docker-postgres-go-rest-api/database"
+)
 
 func main() {
+	database.ConnectDB()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Woadaasdasdsdasd 👋!")
+		return c.SendString("He, Woa 👋!")
 	})
 
 	app.Listen(":3000")
---
+++ database/database.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/melihcanclk/docker-postgres-go-rest-api/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBInstance struct {
+	Db *gorm.DB
+}
+
+var DB DBInstance
+
+func ConnectDB() {
+	dataSourceName := fmt.Sprintf("host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	Db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database, %v", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database")
+	Db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations...")
+
+	// get models that we'll migrate, if we wanna use multiple migration
+	// we can define var models = []interface{}{&User{}, &Product{}, &Order{}}
+	// then db.Automigrate(models...)
+	Db.AutoMigrate(&models.Fact{})
+
+	DB = DBInstance{
+		Db,
+	}
+
+}
